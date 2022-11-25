733ed551e2ab1f442c5d654e799839f3ef5af17f
Aricsprague <94206094+Aricsprague@users.noreply.github.com>
2022-11-25 12:24:41 -0600

    Version 2.1
    -Extracted methods to break code up/make it look cleaner and easier to follow.

--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         //declarations
-        int guess=0;
-
         String playAgain;
         String playHard;
         Scanner getGuess = new Scanner(System.in);
@@ -19,97 +17,128 @@ public class Main {
         System.out.println("Would you like to play hard mode? (y or n)");
         playHard = hardMode.nextLine();
 
-
-
         //replay check
         do{
-        if (playHard.equals("n")){ do {
-            //Generate random number
-            RandomNumber number = new RandomNumber();
+            if (playHard.equals("n")){
+                    do {
+                        //Generate easy number
+                        RandomNumber number = new RandomNumber();
+                        startDialog(playerName, number);
+                        int i;
 
-            System.out.println("Well, " + playerName + ", I am thinking of a number between " + number.min + " and " + number.max + ".");
-            System.out.println("Take a guess.");
+                        //for loop for gameplay
+                        i = guess(getGuess, playerName, number);
 
-            int i =0;
+                        //If statement for player not guessing correctly in 6 tries
+                        lost(number, i);
 
-            //for loop for gameplay
-            for(i=1; i<7; i++){
-                guess = getGuess.nextInt();
+                        playAgain = playAgain(replay);
 
-                if (guess == number.genNumber) {
-                    System.out.println("Good job, " + playerName + "! You guessed my number in " + i + " guesses!");
-                    break;
-                }
-                else if(guess>number.genNumber) {
-                    System.out.println("Your guess is too high.");
-                    System.out.println("Take a guess.");
-                }
-                else {
-                    System.out.println("Your guess is too low.");
-                    System.out.println("Take a guess.");
-                }
+                        if (playAgain.equals("y")){
+                            System.out.println("Would you like a harder number? (y or n)");
+                            playHard = hardMode.nextLine();
+                        }
 
+                    } while (playAgain.equals("y") && playHard.equals("n"));
             }
+            else {
+                do {
+                    //Generate hard number
+                    HarderNumber number = new HarderNumber();
+                    startDialogHard(playerName, number);
+                    int i;
 
-            if (i == 7){
-                System.out.println("Too bad! You didn't guess the number fast enough. :(");
-                System.out.println("The number was: " + number.genNumber);
-            }
+                    //If statement for player not guessing correctly in 6 tries
+                    i = hardGuess(getGuess, playerName, number);
+                    lostHard(number, i);
 
-            System.out.println("Would you like to play again? (y or n)");
-            playAgain = replay.nextLine();
+                    playAgain = playAgain(replay);
 
-            if (playAgain.equals("y")){
-                System.out.println("Would you like a harder number? (y or n)");
-                playHard = hardMode.nextLine();
-            }
-
-    } while (playAgain.equals("y") && playHard.equals("n")); } else {
-            do {
-                //Generate random number
-                HarderNumber number = new HarderNumber();
-
-                System.out.println("Well, " + playerName + ", I am thinking of a number between " + number.min + " and " + number.max + ".");
-                System.out.println("Take a guess.");
-
-                int i =0;
-
-                //for loop for gameplay
-                for(i=1; i<7; i++){
-                    guess = getGuess.nextInt();
-
-                    if (guess == number.genNumber) {
-                        System.out.println("Good job, " + playerName + "! You guessed my number in " + i + " guesses!");
-                        break;
-                    }
-                    else if(guess>number.genNumber) {
-                        System.out.println("Your guess is too high.");
-                        System.out.println("Take a guess.");
-                    }
-                    else {
-                        System.out.println("Your guess is too low.");
-                        System.out.println("Take a guess.");
+                    if (playAgain.equals("y")){
+                         System.out.println("Would you like continue playing hard mode? (y or n)");
+                         playHard = hardMode.nextLine();
                     }
 
-                }
+                }   while (playAgain.equals("y") && playHard.equals("y"));
+            }
 
-                if (i == 7){
-                    System.out.println("Too bad! You didn't guess the number fast enough. :(");
-                    System.out.println("The number was: " + number.genNumber);
-                }
-                System.out.println("Would you like to play again? (y or n)");
-                playAgain = replay.nextLine();
-
-                if (playAgain.equals("y")){
-                    System.out.println("Would you like continue playing hard mode? (y or n)");
-                    playHard = hardMode.nextLine();
-                }
-
-            } while (playAgain.equals("y") && playHard.equals("y"));
-        }
-
-
-
-    } while (playAgain.equals("y"));
+        } while (playAgain.equals("y"));
 }
+
+    private static String playAgain(Scanner replay) {
+        String playAgain;
+        System.out.println("Would you like to play again? (y or n)");
+        playAgain = replay.nextLine();
+        return playAgain;
+    }
+
+    private static void startDialog(String playerName, RandomNumber number) {
+        System.out.println("Well, " + playerName + ", I am thinking of a number between " + number.min + " and " + number.max + ".");
+        System.out.println("Take a guess.");
+    }
+
+    private static void startDialogHard(String playerName, HarderNumber number) {
+        System.out.println("Well, " + playerName + ", I am thinking of a number between " + number.min + " and " + number.max + ".");
+        System.out.println("Take a guess.");
+    }
+
+    private static void lostHard(HarderNumber number, int i) {
+        if (i == 7){
+           System.out.println("Too bad! You didn't guess the number fast enough. :(");
+           System.out.println("The number was: " + number.genNumber);
+        }
+    }
+
+    private static void lost(RandomNumber number, int i) {
+        if (i == 7){
+            System.out.println("Too bad! You didn't guess the number fast enough. :(");
+            System.out.println("The number was: " + number.genNumber);
+        }
+    }
+
+    private static int hardGuess(Scanner getGuess, String playerName, HarderNumber number) {
+        int guess;
+        int i;
+        for(i=1; i<7; i++){
+            guess = getGuess.nextInt();
+
+            if (guess == number.genNumber) {
+                System.out.println("Good job, " + playerName + "! You guessed my number in " + i + " guesses!");
+                break;
+            }
+            else if(guess> number.genNumber) {
+                System.out.println("Your guess is too high.");
+                System.out.println("Take a guess.");
+            }
+            else {
+                System.out.println("Your guess is too low.");
+                System.out.println("Take a guess.");
+            }
+
+        }
+        return i;
+    }
+
+    private static int guess(Scanner getGuess, String playerName, RandomNumber number) {
+        int guess;
+        int i;
+        for(i=1; i<7; i++){
+            guess = getGuess.nextInt();
+
+            if (guess == number.genNumber) {
+                System.out.println("Good job, " + playerName + "! You guessed my number in " + i + " guesses!");
+                break;
+            }
+            else if(guess> number.genNumber) {
+                System.out.println("Your guess is too high.");
+                System.out.println("Take a guess.");
+            }
+            else {
+                System.out.println("Your guess is too low.");
+                System.out.println("Take a guess.");
+            }
+
+        }
+        return i;
+    }
 }
